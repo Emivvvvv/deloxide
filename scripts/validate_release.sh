f5bdff7c5e8e1453eea30c0533c7049ecff18ef6
Emirhan TALA <tala.emirhan@gmail.com>
2026-07-29 13:43:28 +0300

    release(artifacts)

--- scripts/validate_release.sh
+++ scripts/validate_release.sh
@@ -15,4 +15,8 @@ cargo fmt --all -- --check
 cargo clippy --lib --bins --examples --all-features -- -D warnings
 cargo test --lib
 scripts/check_docs.sh
-cargo package
+package_args=()
+if [[ "${DELOXIDE_ALLOW_DIRTY:-0}" == "1" ]]; then
+  package_args+=(--allow-dirty)
+fi
+cargo package "${package_args[@]}"
